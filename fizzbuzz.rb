19b4a6f44806726271c52fc62789c77911897a99
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-08 16:44:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  
+  if number % 3 && number % 5
+    return "FizzBuzz"
+  elsif number % 3
+    return "Fizz"
+  elsif number % 5
+    return "Buzz"
+  else
+    return nil
+  end
 end
